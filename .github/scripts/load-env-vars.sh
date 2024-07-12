f8be4fcb92b2f838a960e37315c2b521edeffd1d
dghama <adgham@mobelite.fr>
2024-07-12 20:24:55 +0100

    changing file config

--- .github/scripts/load-env-vars.sh
+++ .github/scripts/load-env-vars.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+set -e
 
 # Adjust the path to your .env file if necessary
-ENV=$(basename "${GITHUB_REPOSITORY}")
-echo "APP_NAME=${ENV}" >> $GITHUB_ENV
+ENV_FILE=".env"
+
+if [ -f "$ENV_FILE" ]; then
+  while IFS='=' read -r key value; do
+    # Remove any leading/trailing whitespace from key or value
+    key=$(echo $key | tr -d '[:space:]')
+    value=$(echo $value | tr -d '[:space:]')
+    echo "Setting environment variable: $key"
+    echo "$key=$value" >> $GITHUB_ENV
+  done < "$ENV_FILE"
+else
+  echo "Error: $ENV_FILE not found."
+  exit 1
+fi
