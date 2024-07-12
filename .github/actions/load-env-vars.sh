99579787c8877fadc278c0b3e5696a08da2cd515
dghama <adgham@mobelite.fr>
2024-07-12 17:06:09 +0100

    fix env

--- .github/actions/load-env-vars.sh
+++ .github/actions/load-env-vars.sh
@@ -6,6 +6,7 @@ set -e
 ENV_FILE=".env"
 
 if [ -f "$ENV_FILE" ]; then
+  echo "found: $(cat $ENV_FILE | grep -v '^#' | xargs) found."
   export $(cat $ENV_FILE | grep -v '^#' | xargs)
 else
   echo "Error: $ENV_FILE not found."
