32d191261c28af73edc5c23df1b874865794b747
dghama <adgham@mobelite.fr>
2024-08-01 09:24:25 +0100

    feat(ci/cd): remove load env script

--- .github/scripts/load-env-vars.sh
+++ .github/scripts/load-env-vars.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -e
+# set -e
 
-# Adjust the path to your .env file if necessary
-ENV_FILE=".env"
+# # Adjust the path to your .env file if necessary
+# ENV_FILE=".env"
 
-if [ -f "$ENV_FILE" ]; then
-  while IFS='=' read -r key value; do
-    # Remove any leading/trailing whitespace from key or value
-    key=$(echo $key | tr -d '[:space:]')
-    value=$(echo $value | tr -d '[:space:]')
-    echo "Setting environment variable: $key"
-    echo "$key=$value" >> $GITHUB_ENV
-  done < "$ENV_FILE"
-else
-  echo "Error: $ENV_FILE not found."
-  exit 1
-fi
+# if [ -f "$ENV_FILE" ]; then
+#   while IFS='=' read -r key value; do
+#     # Remove any leading/trailing whitespace from key or value
+#     key=$(echo $key | tr -d '[:space:]')
+#     value=$(echo $value | tr -d '[:space:]')
+#     echo "Setting environment variable: $key"
+#     echo "$key=$value" >> $GITHUB_ENV
+#   done < "$ENV_FILE"
+# else
+#   echo "Error: $ENV_FILE not found."
+#   exit 1
+# fi
 
