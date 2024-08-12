d5143f0029d55cb8c669cc66f2502fc0bfa3b8ed
dghama <adgham@mobelite.fr>
2024-08-12 13:52:20 +0100

    feat(ci/cd): fix pumping verison

---
+++ fastlane/actions/update_version_name.rb
@@ -0,0 +1,20 @@
+require 'json'
+
+# Load the package.json file
+package_file = File.read('./package.json')
+package_data = JSON.parse(package_file)
+
+# Get the version from package.json
+new_version = package_data['version']
+new_version_with_app_name = "#{package_data['name']}-#{new_version}#{ENV['ENVIRONMENT'] || 'dev'}"
+
+# Update the build.gradle file
+build_gradle_path = './android/app/build.gradle'
+if File.exist?(build_gradle_path)
+  file_content = File.read(build_gradle_path)
+  updated_content = file_content.gsub(/versionName ".*"/, "versionName \"#{new_version_with_app_name}\"")
+  File.open(build_gradle_path, 'w') { |file| file.write(updated_content) }
+  puts "Updated Android versionName to #{new_version_with_app_name}"
+else
+  puts "build.gradle file does not exist at #{build_gradle_path}"
+end
